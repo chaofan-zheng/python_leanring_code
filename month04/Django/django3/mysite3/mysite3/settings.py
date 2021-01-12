0733cf43c70710a58ad75739ed21d70570ea0928
chaofan-zheng <417355570@qq.com>
2021-01-12 14:39:45 +0800

    update

--- month04/Django/django3/mysite3/mysite3/settings.py
+++ month04/Django/django3/mysite3/mysite3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'music',
     'user',
+    'bookstore',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'mysite3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite3',
+        'USER': 'root',
+        'PASSWORD': '417355570',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
